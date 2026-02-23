559dd017c66418cb59faa36bba01e671ea1897dd
Jose F. Rives Lirola <jose@digio.es>
2026-02-23 23:26:08 +0100

    fix: warning with vue-i18n

--- app/vite.config.js
+++ app/vite.config.js
@@ -53,6 +53,9 @@ export default defineConfig(async () => {
 	return {
 		define: {
 			__BRIO_VERSION__: JSON.stringify(version),
+			__VUE_I18N_FULL_INSTALL__: true,
+			__VUE_I18N_LEGACY_API__: false,
+			__INTLIFY_PROD_DEVTOOLS__: false,
 		},
 		plugins: [
 			brioExtensions(),
